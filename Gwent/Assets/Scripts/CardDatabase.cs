1c87e1cb568c5a76274af3b6f0e1085f5fc70325
ramonchg04 <157727936+ramonchg04@users.noreply.github.com>
2024-04-19 11:00:52 -0400

    fixes

--- Gwent/Assets/Scripts/CardDatabase.cs
+++ Gwent/Assets/Scripts/CardDatabase.cs
@@ -6,5 +6,10 @@ public class CardDatabase : MonoBehaviour
 {
     public static List<Card> Cards = new List<Card>();// Una lista donde vamos a almacenar las cartas del juego
 
+    void Awake()
+    {
 
+        Cards.Add(new Card(1,"Reina Arquera","Líder", "Mantener una carta aleatoria en el campo entre ronda y ronda"));   
+    
+    }
 }
