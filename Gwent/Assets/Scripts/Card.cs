475f73e2a8c980c0710ece0c8ec6ab4881cfd571
ramonchg04 <157727936+ramonchg04@users.noreply.github.com>
2024-04-19 10:50:47 -0400

    Creating cardDatabase

---
+++ Gwent/Assets/Scripts/Card.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class Card //Creando mi clase carta que contendra todas las caracteristicas comunes de las cartas
+{
+    public int id;
+    public string cardName;
+    public string cardType;
+    public string cardEffect;
+    
+    public Card()
+    {
+
+    }
+
+    public Card(int Id, string CardName, string CardType, string CardEffect) //Metodo contructor de esta clase
+    {
+        this.id = Id;
+        this.cardName = CardName;
+        this.cardType = CardType;
+        this.cardEffect = CardEffect;
+    }   
+}
+
+public class Hero : Card//Creo la clase para las cartas de tipo heroe, que hereda de la clase carta
+{
+    public string attackType;
+    public int power;
+
+    public Hero(int Id, string CardName, string CardType, string CardEffect,int Power, string AttackType) //Metodo contructor de esta clase
+    {
+        this.id = Id;
+        this.cardName = CardName;
+        this.cardType = "Héroe";
+        this.cardEffect = CardEffect;
+        this.power = Power;
+        this.attackType = AttackType;
+    }
+}
+
+public class Silver : Card//Creo la clase para las cartas de tipo plata, que hereda de la clase carta
+{
+    public string attackType;
+    public int power;
+
+    public Silver(int Id, string CardName, string CardType, string CardEffect, int Power, string AttackType) //Metodo contructor de esta clase
+    {
+        this.id = Id;
+        this.cardName = CardName;
+        this.cardType = "Plata";
+        this.cardEffect = CardEffect;
+        this.power = Power;
+        this.attackType = AttackType;
+    }
+}
+
+public class Decoy : Card//Creo la clase para las cartas de tipo señuelo, que hereda de la clase carta
+{
+    public int power;
+
+    public Decoy(int Id, string CardName, string CardType, string CardEffect, int Power) //Metodo contructor de esta clase
+    {
+        this.id = Id;
+        this.cardName = CardName;
+        this.cardType = "Señuelo";
+        this.cardEffect = CardEffect;
+        this.power = 0;
+    }
+}
---
+++ Gwent/Assets/Scripts/CardDatabase.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDatabase : MonoBehaviour
+{
+    public static List<Card> Cards = new List<Card>();// Una lista donde vamos a almacenar las cartas del juego
+
+
+}
